cf9c2062fe10b5ea2ee74c4526bccd84444e2fe8
Daksh Shah <dakshah@ucsc.edu>
2023-08-28 16:14:25 -0700

    started setting up model trainer

---
+++ HandTrain.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+import os
+from mediapipe_model_maker import gesture_recognizer
+import matplotlib.pyplot as plt
+
+
+# https://developers.google.com/mediapipe/solutions/customization/gesture_recognizer
+
+# insert training and test data import here
+
